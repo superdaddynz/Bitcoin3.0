02c3c56f1565597d1da68831a20e59af22786b80
sdnz <superdaddynz@gmail.com>
2021-03-23 00:50:07 +1300

    Fix stale blockchain
    With high minging difficulty attack, the chain is stale for hours.
    To prevent halve difficulty in every 50 minutes in stale chain.
    Also make timespan to use 2 blocks correctly.

--- src/pow.cpp
+++ src/pow.cpp
@@ -11,8 +11,17 @@
 #include <primitives/block.h>
 #include <uint256.h>
 
+int64_t HF1_BLOCK_HEIGHT = 1842;
+
+unsigned int GetNextWorkRequired_HF1(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
+unsigned int CalculateNextWorkRequired_HF1(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
+unsigned int CalculateNextWorkRequiredStale(const CBlockIndex* pindexLast, int shift, const Consensus::Params&);
+
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
+    if ((pindexLast->nHeight+1) >= HF1_BLOCK_HEIGHT)
+        return GetNextWorkRequired_HF1(pindexLast, pblock, params);
+
     assert(pindexLast != nullptr);
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
@@ -68,6 +77,95 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 
     if (bnNew > bnPowLimit)
         bnNew = bnPowLimit;
+
+    return bnNew.GetCompact();
+}
+
+unsigned int GetNextWorkRequired_HF1(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
+{
+    assert(pindexLast != nullptr);
+    unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
+
+    // If the blockchain is stale for long time, halve the difficulty in every 50 minutes.
+    if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*10)
+    {
+        return CalculateNextWorkRequiredStale(pindexLast, (pblock->GetBlockTime() - pindexLast->GetBlockTime()) / params.nPowTargetSpacing*10, params);
+    }
+
+    // Only change once per difficulty adjustment interval
+    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
+    {
+        if (params.fPowAllowMinDifficultyBlocks)
+        {
+            // Special difficulty rule for testnet:
+            // If the new block's timestamp is more than 2* 5 minutes
+            // then allow mining of a min-difficulty block.
+            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
+                return nProofOfWorkLimit;
+            else
+            {
+                // Return the last non-special-min-difficulty-rules-block
+                const CBlockIndex* pindex = pindexLast;
+                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
+                    pindex = pindex->pprev;
+                return pindex->nBits;
+            }
+        }
+        return pindexLast->nBits;
+    }
+
+    int nHeightFirst = pindexLast->nHeight - params.DifficultyAdjustmentInterval();
+    assert(nHeightFirst >= 0);
+    const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
+    assert(pindexFirst);
+
+    return CalculateNextWorkRequired_HF1(pindexLast, pindexFirst->GetBlockTime(), params);
+}
+
+unsigned int CalculateNextWorkRequired_HF1(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
+{
+    if (params.fPowNoRetargeting)
+        return pindexLast->nBits;
+
+    // Limit adjustment step
+    int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
+    if (nActualTimespan < params.nPowTargetTimespan/2)
+        nActualTimespan = params.nPowTargetTimespan/2;
+    if (nActualTimespan > params.nPowTargetTimespan*2)
+        nActualTimespan = params.nPowTargetTimespan*2;
+
+    // Retarget
+    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
+    arith_uint256 bnNew;
+    bnNew.SetCompact(pindexLast->nBits);
+    bnNew *= nActualTimespan;
+    bnNew /= params.nPowTargetTimespan;
+
+    if (bnNew > bnPowLimit)
+        bnNew = bnPowLimit;
+
+    return bnNew.GetCompact();
+}
+
+unsigned int CalculateNextWorkRequiredStale(const CBlockIndex* pindexLast, int shift, const Consensus::Params& params)
+{
+    if (params.fPowNoRetargeting)
+        return pindexLast->nBits;
+
+    // Retarget
+    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
+    arith_uint256 bnNew;
+    bnNew.SetCompact(pindexLast->nBits);
+
+    for (int i=0; i<shift; i++)
+    {
+        bnNew *= 2;
+        if (bnNew > bnPowLimit)
+        {
+            bnNew = bnPowLimit;
+            break;
+        }
+    }
 
     return bnNew.GetCompact();
 }
